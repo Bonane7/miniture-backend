27cf96590da2f4e87f9685cb79b348652ec67c2c
Bonane Busoke <bonanebusoke@gmail.com>
2026-06-11 17:41:22 +0200

    same modification in usercontroller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,7 +3,7 @@ import { accessToken } from "../utils/genereteToken.js";
 import bcrypt from "bcryptjs";
 
 // REGISTER USER
-  //etape: coll filds user.body ,if user existe, hash password, creat password, save,Generate token, user Response,
+
 export const register = async (req, res) => {
   try {
     const { FirstName, LastName, email, password, userRole } = req.body;
@@ -105,85 +105,3 @@ export const login = async (req, res) => {
     });
   }
 };
-
-// import User from "../model/userModel.js";
-// import bcrypt from "bcryptjs";
-// import jwt from "jsonwebtoken";
-// import dotenv from "dotenv"
-
-// dotenv.config()
-
-// export const registerUser = async (req, res) => {
-//   try {
-//     const { userName, email, password } = req.body;
-
-//     if (!userName || !email || !password) {
-//       return res.status(400).json({ message: "All fields are required" });
-//     }
-
-//     //hash pasword
-//     const salt = await bcrypt.genSalt(10);
-//     const hashedPassword = await bcrypt.hash(password, salt);
-
-//     const user = new User({
-//       userName,
-//       email,
-//       password: hashedPassword,
-//     });
-
-//     await user.save();
-//     res.status(201).json({
-//       message: "User created successfully",
-//       data: {
-//         userName: user.userName,
-//         email: user.email,
-//         id: user._id,
-//         createdAt: user.createdAt,
-//       },
-//     });
-//   } catch (error) {
-//     res
-//       .status(500)
-//       .json({ message: "Error creating use", error: error.message });
-//   }
-// };
-
-// export const loginUser = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-
-//     if (!email || !password) {
-//       return res.status(400).json({
-//         message: "All fields are required",
-//       });
-//     }
-//     //SEARCH IN DATABASE
-//     const user = await User.findOne({ email });
-//     if (!user) {
-//       return res.status(400).json({ message: "user not fund" });
-//     }
-//     //COMPAR PASSWORD
-//     const isMatch = await bcrypt.compare(password, user.password);
-//     if (!isMatch) {
-//       return res.status(400).json({
-//         message: "invalide credential",
-//       });
-//     }
-//     //CREAT TOCKEN
-//     const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET , {
-//       expiresIn: process.env.JW_EXPIRES_IN,
-//     });
-
-//     res.status(200).json({
-//       message: "User connected successfuly",
-//       token,
-//       user: {
-//         id: user._id,
-//         userName: user.userName,
-//         email: user.email,
-//       },
-//     });
-//   } catch (error) {
-//     res.status(500).json({ message: "error login user", error: error.message });
-//   }
-// };
